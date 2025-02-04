7a64024eeeb079805883795bd920ab4fcf7d5d35
梦归云帆 <1138663075@qq.com>
2025-02-05 00:59:30 +0800

    优化nuitka构建流程

--- build_exe.py
+++ build_exe.py
@@ -1,6 +1,28 @@
 import os
+from colorama import Fore, Back, Style, init, deinit
+
+# doc:https://nuitka.net/user-documentation/user-manual.html
+
 
 if __name__ == "__main__":
-    command = "nuitka ./Manager.py --msvc=latest --mode=standalone"
-    print(f"running command: {command}")
-    os.system(command)
+    init()
+
+    EXE_NAME = "wxWallPaperEngine"
+
+    command_list = [
+        rf"nuitka ./{EXE_NAME}.py"
+        + rf" --msvc=latest"
+        + rf" --mode=onefile"  # --mode=standalone/onefile/app/accelerated
+        + rf" -o {EXE_NAME}"
+        + rf" --report=compilation-report.xml"
+        + rf" --include-data-dir=./icon=./icon"
+        + rf" --lto=yes",
+    ]
+
+    for index, command in enumerate(command_list):
+        print(
+            f"{Fore.YELLOW}Building wxPython_WallpaperEngine: running command [{index}]: {Fore.BLUE}{command}{Fore.RESET}"
+        )
+        os.system(command)
+
+    deinit()
